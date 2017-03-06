59f083ed26a32d2b1bb87caca8996d3f205159df
Maia <maia.rt.46@gmail.com>
2017-03-06 18:27:31 -0500

    Aun no he terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,10 @@ var MensajeS3 = function(divtexte) {
 		divtext.style.display = "block";
 	}
 }
+//Funcion Coders
+
+
+
 // Funcion Dropdown
 function myFunction() {
 	document.getElementById("myDropdown").classList.toggle("show");
@@ -31,15 +35,57 @@ window.onclick = function(event) {
 	if (!event.target.matches('.dropbtn')) {
 
 		var dropdowns = document.getElementsByClassName("dropdown-content");
-		var i;
+		var i ;
 		for (i = 0; i < dropdowns.length; i++) {
 			var openDropdown = dropdowns[i];
+	
 			if (openDropdown.classList.contains('show')) {
+
 				openDropdown.classList.remove('show');
+
+				var imprimir =document.getElementById('imprimir');
+               
+
+    var datos =
+  [ { nombre : "Meche", apellido : "Zubieta", rol : "1era Promoción", status : "Trabajando" },
+    { nombre : "Arabela", apellido : "Rojas", rol : "2da Promocion", status : "Trabajando" },
+    { nombre : "Guadalupe", apellido : "Racio", rol : "3ra Promoción", status : "Trabajando" },
+    { nombre : "Daniella", apellido : "Durán", rol : "4ta Promoción", status : "Trabajando" },
+    { nombre : "Michelle", apellido : "Seguil", rol : "5ta Promoción", status : "Trabajando" },
+    { nombre : "Diana", apellido : "Navarro", rol : "5ta Promoción", status : "Trabajando" } ];
+
+    imprimir.innerHTML = "";
+    datos.forEach(function(elem){
+      imprimir.innerHTML += "<div><ul><li>"+ "Nombre: "+ elem.nombre + "</li>"+
+      "<li>"+ "Apellido: "+ elem.apellido + "</li>"+
+      "<li>"+ "Rol: "+ elem.rol + "</li>"+
+      "<li>"+ "Cumpleaños: "+ elem.status + "</li></ul></div>" ;
+    } ); 
 			}
 		}
 	}
 }
+
+ /*var imprimir =document.getElementById('imprimir');
+var valorA = document.getElementsByTagName('a')[0];
+ console.log(valorA);
+
+    var datos =
+  [ { nombre : "Meche", apellido : "Zubieta", rol : "1era Promoción", status : "Trabajando" },
+    { nombre : "Arabela", apellido : "Rojas", rol : "2da Promocion", status : "Trabajando" },
+    { nombre : "Guadalupe", apellido : "Racio", rol : "3ra Promoción", status : "Trabajando" },
+    { nombre : "Daniella", apellido : "Durán", rol : "4ta Promoción", status : "Trabajando" },
+    { nombre : "Michelle", apellido : "Seguil", rol : "5ta Promoción", status : "Trabajando" },
+    { nombre : "Diana", apellido : "Navarro", rol : "5ta Promoción", status : "Trabajando" } ];
+
+    imprimir.innerHTML = "";
+    datos.forEach(function(elem){
+      valor.innerHTML += "<div><ul><li>"+ "Nombre: "+ elem.nombre + "</li>"+
+      "<li>"+ "Apellido: "+ elem.apellido + "</li>"+
+      "<li>"+ "Rol: "+ elem.rol + "</li>"+
+      "<li>"+ "Cumpleaños: "+ elem.status + "</li></ul></div>" ;
+    } ); }
+    else{ alert('holii'); }; */
 
 
 
