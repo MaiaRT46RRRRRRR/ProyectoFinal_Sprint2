bb6393163cea359df41fbd1dc42d4e1eb7f9f981
Maia <maia.rt.46@gmail.com>
2017-03-06 16:16:16 -0500

    Suubiendo propiedad botones

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+var MensajeS1 = function(divtexte) {
+	var divtext = document.getElementById(divtexte);
+	if (divtext.style.display == "block" ) {
+		divtext.style.display = "none";
+	} else{
+		divtext.style.display = "block";
+	}
+}
+var MensajeS2 = function(divtexte) {
+	var divtext = document.getElementById(divtexte);
+	if (divtext.style.display == "block" ) {
+		divtext.style.display = "none";
+	} else{
+		divtext.style.display = "block";
+	}
+}
+var MensajeS3 = function(divtexte) {
+	var divtext = document.getElementById(divtexte);
+	if (divtext.style.display == "block" ) {
+		divtext.style.display = "none";
+	} else{
+		divtext.style.display = "block";
+	}
+}
+
+
+
+
+
+
+
